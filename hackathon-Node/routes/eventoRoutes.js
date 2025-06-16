d0deb70fc3f6ee6682d2f5b3a7edb8ec6ae00515
Fernando Cardoso <cardosofernando112@gmail.com>
2025-06-16 02:34:47 -0300

    Delete hackathon-Node directory

--- hackathon-Node/routes/eventoRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/eventoController');
-
-router.get('/:id', controller.getEventoById);
-module.exports = router;
